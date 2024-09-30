e19d72488e8ffcababdefb96a4eefcf15f4a9a7f
JovianP <44480402+PedroParreiras@users.noreply.github.com>
2024-09-30 16:26:28 -0300

    Update firebase.js

--- uay/src/firebase.js
+++ uay/src/firebase.js
@@ -1,29 +1,26 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 
-
-// Substitua as configurações abaixo pelas suas configurações do Firebase
+// Use environment variables for Firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyA51Gl_50ROX39Uhs3Q0IXf7s1qtAL4_Hc",
-    authDomain: "y-uay-e375e.firebaseapp.com",
-    projectId: "y-uay-e375e",
-    storageBucket: "y-uay-e375e.appspot.com",
-    messagingSenderId: "144096183950",
-    appId: "1:144096183950:web:5262ad793387968cd86bdf",
-    measurementId: "G-W6RFR52NWM"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
-// Inicializar Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Inicializar serviços
+// Initialize services
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app); // Adicionando Storage
+const storage = getStorage(app);
 
-
-// Exportar os serviços para uso em outros arquivos
+// Export services
 export { auth, db, storage };
